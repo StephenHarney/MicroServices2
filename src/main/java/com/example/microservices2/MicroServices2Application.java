d641ccfe427c33055bc4da77a93ea44fe7ec591e
Stephen Harney <g00367573@atu.ie>
2023-10-05 15:49:12 +0100

    Initial commit

---
+++ src/main/java/com/example/microservices2/MicroServices2Application.java
@@ -0,0 +1,13 @@
+package com.example.microservices2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServices2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServices2Application.class, args);
+    }
+
+}
