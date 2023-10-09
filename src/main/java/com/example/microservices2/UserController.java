f0deaee6e687f557a7036b367f5658fa870f56e4
Stephen Harney <g00367573@atu.ie>
2023-10-09 18:58:01 +0100

    Got it to get vaules from the link.

--- src/main/java/com/example/microservices2/UserController.java
+++ src/main/java/com/example/microservices2/UserController.java
@@ -1,4 +1,35 @@
 package com.example.microservices2;
 
-public class UserController {
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController  {
+
+    private UserService emailService;
+
+
+    @Autowired
+    public UserController(UserService emailService){
+        this.emailService = emailService;
+    }
+
+    @GetMapping("/registerUser/{name}/{email}")
+    public String getUser(@PathVariable String name, @PathVariable String email) {
+    emailService.sendEmail(name,email);
+    return "Please Register here";
+
+
+
+    //links: http://localhost:8080/registerUser/name/email
+}
+
+
+
+
+
+
 }
--- src/main/java/com/example/microservices2/UserService.java
+++ src/main/java/com/example/microservices2/UserService.java
@@ -1,7 +1,12 @@
 package com.example.microservices2;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+
+    public void sendEmail(String name, String message){
+
+    }
 }
-
-
-
