ab1f1cb8e14f9d2fbaba5228d1d25d33f10ff717
Stephen Harney <g00367573@atu.ie>
2023-10-09 19:06:39 +0100

    update

--- src/main/java/com/example/microservices2/UserController.java
+++ src/main/java/com/example/microservices2/UserController.java
@@ -3,28 +3,14 @@ package com.example.microservices2;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController  {
+    @PatchMapping("/RegisterUserBody")
 
-    private UserService emailService;
-
-
-    @Autowired
-    public UserController(UserService emailService){
-        this.emailService = emailService;
-    }
-
-    @GetMapping("/registerUser/{name}/{email}")
-    public String getUser(@PathVariable String name, @PathVariable String email) {
-    emailService.sendEmail(name,email);
-    return "Please Register here";
-
-
-
-    //links: http://localhost:8080/registerUser/name/email
 }
 
 
