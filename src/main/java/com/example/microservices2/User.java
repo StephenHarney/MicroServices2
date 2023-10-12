95f9ab19e490b1fa3c6b18312dbf0a4314b2fcae
Stephen Harney <g00367573@atu.ie>
2023-10-12 15:57:56 +0100

    Update

--- src/main/java/com/example/microservices2/User.java
+++ src/main/java/com/example/microservices2/User.java
@@ -3,12 +3,14 @@ package com.example.microservices2;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Data//creates getters and setters and filed methods
+@NoArgsConstructor//creates default constructors
+@AllArgsConstructor//Sorts out all other constructors that is possible
+
 
 public class User {
     private String userName;
-    private String emails;
+    private String email;
 }
--- src/main/java/com/example/microservices2/UserController.java
+++ src/main/java/com/example/microservices2/UserController.java
@@ -2,17 +2,16 @@ package com.example.microservices2;
 
 
 import org.apache.catalina.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController  {
-    @PatchMapping("RegisterUserBody")
-    @ResponseBody(HttpStatus.CREATED)
-    public User registerUserCredentials(@RequestBody User user){
+    @PostMapping("registerUserBody")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User registerUserBody(@RequestBody User user){
          System.out.println("User ID" +user.getUsername());
-         System.out.println("User ID:"+user.getClass());
+         System.out.println("User ID:"+user.getPassword());
          return user;
 
     }
@@ -25,4 +24,4 @@ public class UserController  {
 
 
 
-}
+
